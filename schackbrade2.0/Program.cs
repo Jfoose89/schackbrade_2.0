dc8ca60b43e86eaa8a6162a98763c087d9c01def
JFoose89 <jordan.foose@chasacademy.se>
2025-09-22 21:57:24 +0200

    Initial project setup: console app created for chessboard assignment

---
+++ schackbrade2.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace schackbrade2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
